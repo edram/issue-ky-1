06991de932a4f3372381f0a6bfe26c31d6968fa3
edram <edram@qq.com>
2024-04-25 14:13:02 +0800

    复现代码

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,15 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import VConsole from "vconsole";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+new VConsole();
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+console.log(1);
+const request = new Request("https://empty.invalid", {
+  body: new ReadableStream(),
+  method: "POST",
+  // @ts-expect-error - Types are outdated.
+  get duplex() {
+    return "half";
+  },
+});
+
+console.log(request);
